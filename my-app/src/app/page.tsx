632a1ac5b90f08322a2e526a17241d75f9b49f24
cristiangar0398 <cristianagr1998@gmail.com>
2024-07-12 23:52:16 -0500

    navBar finished

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,9 +4,6 @@ import NavBar from '../components/shared/header/navBar'
 export default function Home() {
   return (
     <div>
-      <h1>julian gay</h1>
-      <h1 id='servicios'>julian gay2</h1>
-      <h1 id='servicios2'>julian gay2</h1>
     </div>
     
   );
