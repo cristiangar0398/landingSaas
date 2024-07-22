0e52806309dfd1eef09c8d2fd3024259752a2290
cristiangar0398 <cristianagr1998@gmail.com>
2024-07-21 23:35:21 -0500

    Email functionality completed, styles in progress

--- my-app/src/app/api/send-email/route.ts
+++ my-app/src/app/api/send-email/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 import { sendEmail } from '../../../services/emailService';
 
 export async function POST(request: Request) {
-  const { subject, text } = await request.json();
+  const { firstName, phone, email, message } = await request.json();
 
   try {
-    await sendEmail({ subject, text });
+    await sendEmail({ firstName, phone, email, message });
     return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: 'Error sending email', error }, { status: 500 });
